b132b347f1dc4078cfefcaee9df71471002f1b2b
Wrech <94825128+Wrech1001@users.noreply.github.com>
2022-12-30 16:50:58 +0700

    Update Fake Virus.cpp

--- Fake Virus.cpp
+++ Fake Virus.cpp
@@ -4,8 +4,8 @@
 
 int main()
 {
-    std::cout << "Hacked\n";
     ShowWindow(::GetConsoleWindow(), SW_HIDE);
     MessageBox(NULL, L"Fatal error", L"May your computer has virus!", MB_ICONERROR | MB_OK);
     main();
 }
+
