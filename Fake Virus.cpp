18a81671beeac426eb7fcf8a1468e763db06b2ee
Wrech <94825128+Wrech1001@users.noreply.github.com>
2023-02-18 21:54:09 +0700

    Update Fake Virus.cpp

--- Fake Virus.cpp
+++ Fake Virus.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <windows.h>
 
-int main()
-{
+int main() {
 while (true) {
     ShowWindow(GetConsoleWindow(), SW_HIDE);
     MessageBox(NULL, L"Fatal error", L"May your computer has virus!", MB_ICONERROR | MB_OK);
